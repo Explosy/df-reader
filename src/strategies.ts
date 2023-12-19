006ea576fa7c63ca4658f82f6183196ced650099
amoroz <amoroz@onellect.ru>
2023-12-19 08:44:37 +0300

    readMethodShouldReturnBdfFileIfTypeIsBdf => PASS readMethodShouldReturnGdfFileIfTypeIsGdf => PASS

--- src/strategies.ts
+++ src/strategies.ts
@@ -1,5 +1,5 @@
-import type { EdfHeaders, IReadStrategy, Signal } from './types'
-import { EdfFile } from './files'
+import type { BdfHeaders, EdfHeaders, GdfHeaders, IReadStrategy, Signal } from './types'
+import { BdfFile, EdfFile, GdfFile } from './files'
 
 export class EdfStrategy implements IReadStrategy {
   async read(): Promise<EdfFile> {
@@ -7,12 +7,12 @@ export class EdfStrategy implements IReadStrategy {
   }
 }
 export class BdfStrategy implements IReadStrategy {
-  read(): Promise<any> {
-    return Promise.resolve()
+  async read(): Promise<BdfFile> {
+    return new BdfFile({} as BdfHeaders, new Array<Signal>())
   }
 }
 export class GdfStrategy implements IReadStrategy {
-  read(): Promise<any> {
-    return Promise.resolve()
+  async read(): Promise<GdfFile> {
+    return new GdfFile({} as GdfHeaders, new Array<Signal>())
   }
 }
